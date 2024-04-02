2807fa65fb4065516fcdfca38acfd7e744a80bde
Bruno Bomfim Lima <81599298+Bruno23111@users.noreply.github.com>
2024-04-01 21:36:17 -0300

    .

--- sagradomaps/android/app/src/main/kotlin/com/example/sagradomaps/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sagradomaps
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- sagradomaps/sagradomaps/android/app/src/main/kotlin/com/unisagrado/sagradomaps/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.unisagrado.sagradomaps
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
